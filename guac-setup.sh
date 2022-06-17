82cacc47c2305b51993c07d31627d9a4b8c2a649
itiligent <94789708+itiligent@users.noreply.github.com>
2022-06-18 02:24:48 +1000

    updated

--- guac-setup.sh
+++ guac-setup.sh
@@ -28,9 +28,12 @@ wget -O  install-ssl-self-signed-for-nginx.sh https://raw.githubusercontent.com/
 ##Config info
 wget -O  Guac-Completion-Tasks.doc https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/Guac-Completion-Tasks.doc
 
+#Main installer script
+wget -O install-guacamole1.4.0-and-nginx.sh https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/install-guacamole1.4.0-and-nginx.sh
+
 chmod +x *.sh
 
-wget -O install-guacamole1.4.0-and-nginx.sh https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/install-guacamole1.4.0-and-nginx.sh -v -O install-guacamole1.4.0-and-nginx.sh && sudo ./install-guacamole1.4.0-and-nginx.sh
+sudo ./install-guacamole1.4.0-and-nginx.sh
 
 
 
