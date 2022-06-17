6251ff7738c7e444d1ebcb59a02a689695f00a4a
itiligent <94789708+itiligent@users.noreply.github.com>
2022-06-18 02:09:21 +1000

    updated

--- guac-setup.sh
+++ guac-setup.sh
@@ -7,29 +7,29 @@
 ######################################################################### 
 
 ## Auth extensions
-wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/add-duo-mfa-guacamole.sh
-wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/guacamole-auth-duo-1.4.0.jar
-wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/add-totp-mfa-guacamole.sh
-wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/guacamole-auth-totp-1.4.0.jar
-wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/add-ldap-auth-guacamole.sh
-wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/guacamole-auth-ldap-1.4.0.jar
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/add-duo-mfa-guacamole.sh
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/guacamole-auth-duo-1.4.0.jar
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/add-totp-mfa-guacamole.sh
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/guacamole-auth-totp-1.4.0.jar
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/add-ldap-auth-guacamole.sh
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/guacamole-auth-ldap-1.4.0.jar
 
 ##branding extention
-wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/branding.jar
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/branding.jar
 
 ##SSH configs
-wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/create-putty-ssh-keys.sh
-wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/disable-ssh-password-auth.sh
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/create-putty-ssh-keys.sh
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/disable-ssh-password-auth.sh
 
 ##SSL
-wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/install-ssl-letsencrypt-for-nginx.sh
-wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/install-ssl-self-signed-for-nginx.sh
+wget--no-check-certificate --content-disposition  https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/install-ssl-letsencrypt-for-nginx.sh
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/install-ssl-self-signed-for-nginx.sh
 
 ##Config info
-wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/Guac-Completion-Tasks.doc
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/Guac-Completion-Tasks.doc
 
 chmod +x *.sh
 
-sudo wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/install-guacamole1.4.0-and-nginx.sh -v -O install-guacamole1.4.0-and-nginx.sh && ./install-guacamole1.4.0-and-nginx.sh
+sudo --no-check-certificate --content-disposition wget https://raw.githubusercontent.com/itiligent/Guacamole-With-Nginx-Build/main/install-guacamole1.4.0-and-nginx.sh -v -O install-guacamole1.4.0-and-nginx.sh && ./install-guacamole1.4.0-and-nginx.sh
 
 
