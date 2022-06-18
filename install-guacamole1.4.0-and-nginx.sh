a42c83cc81c57c09e699bb16c7dbd5c3f63f4f6a
itiligent <94789708+itiligent@users.noreply.github.com>
2022-06-18 18:21:01 +1000

    updates

--- install-guacamole1.4.0-and-nginx.sh
+++ install-guacamole1.4.0-and-nginx.sh
@@ -43,7 +43,7 @@ sudo systemctl restart systemd-hostnamed
 while true
 do
 	echo   
-	read -p "Enter Nginx reverse proxy FQDN DNS name e.g. site.lan,  : " website
+	read -p "Enter Nginx reverse proxy FQDN DNS name e.g. site.lan  : " website
 	echo -e "Please Note: The same proxy site name must be used when running provided SSL install scripts"
 	echo 	
     break
