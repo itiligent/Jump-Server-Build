ca07a1d222c05072c6929de946d28557f2a5e5b4
itiligent <94789708+itiligent@users.noreply.github.com>
2022-06-19 10:09:29 +1000

    updated

--- install-ssl-self-signed-nginx.sh
+++ install-ssl-self-signed-nginx.sh
@@ -35,6 +35,8 @@ echo $proxysite > /dev/shm/getdomain.txt
 sed -i 's/'$HOSTNAME.'//' /dev/shm/getdomain.txt
 cn=$(cat /dev/shm/getdomain.txt)
 
+cp /etc/nginx/sites-enabled/${proxysite} ~/${proxysite}.bak
+
 echo
 cat <<EOF | sudo tee -a extfile.cnf
 [req]
@@ -134,7 +136,7 @@ showastext7='$http_connection'
 showastext8='\'
 
 # Backup the current Nginx config
-cp /etc/nginx/sites-enabled/$proxysite /home/$HOME/$proxysite.bak
+
 echo 
 echo -e "${cyan}Existing Nginx proxy site config backed up to ~/$proxysite.bak"
 echo 
