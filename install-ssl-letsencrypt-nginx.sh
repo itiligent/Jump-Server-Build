95b0db8adb41569a04dec99705d1fefa96990fe9
itiligent <94789708+itiligent@users.noreply.github.com>
2022-07-03 15:12:24 +1000

    colour fix and remove cleanup

--- install-ssl-letsencrypt-nginx.sh
+++ install-ssl-letsencrypt-nginx.sh
@@ -32,7 +32,7 @@ clear
 # Backup the current Nginx config
 cp /etc/nginx/sites-enabled/${proxysite} ~/${proxysite}.bak
 echo 
-echo -e "${cyan}Existing Nginx proxy site config backed up to ~/$proxysite.bak"
+echo -e "${CYAN}Existing Nginx proxy site config backed up to ~/$proxysite.bak"
 echo 
 echo 
 
@@ -97,6 +97,3 @@ systemctl restart nginx
 # Run certbot
 certbot --nginx -n -d $website --email $certbotemail --agree-tos --redirect --hsts
 
-# Clean up
-rm /dev/shm/getdomain.txt
-rm $proxysite
